d7696d5ee22ce2f121459ab7bb186f887733ea35
kudurlagoutham <119496983+kudurlagoutham@users.noreply.github.com>
2025-12-30 15:30:51 +0530

    added user defined button feature and CLI
    Added new user defined button feature. And by this feature the user can define his own mouse key. And created CLI executable file so that the user can access this feature without running the script over and over again.

--- mouse_key.py
+++ mouse_key.py
@@ -1,6 +1,17 @@
 import keyboard #keyboard
 import mouse #Mouse
 
+print('Hello welcome to the touch pad drawing tool')
+def choose_the_key():
+    print('Press the button that you wanted to replace with the right click of mouse')
+    print('if you wanted to stop this tool press esc')
+    event = keyboard.read_event()  # waits for a key event
+    if event.event_type == keyboard.KEY_DOWN:
+        print(f"Key pressed: {event.name}")
+        return event.name
+    
+button = choose_the_key()
+
 ctrl_pressed = False #key pressed status
 
 def press_ctrl(e):
@@ -18,8 +29,8 @@ def release_ctrl(e):
         #print("Left Ctrl released → Mouse button up")
 
 # Bind Left Ctrl press/release
-keyboard.on_press_key("ctrl", press_ctrl)
-keyboard.on_release_key("ctrl", release_ctrl)
+keyboard.on_press_key(button, press_ctrl)
+keyboard.on_release_key(button, release_ctrl)
 
 # Keep the program running
 keyboard.wait("esc")  # Press Esc to exit
